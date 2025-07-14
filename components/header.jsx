7dcde8f00478b7584c4ab06c754047ca4231538e
Keshav9835 <kbmve2003@gmail.com>
2025-07-14 19:00:58 +0530

    Updated the logo

--- components/header.jsx
+++ components/header.jsx
@@ -20,7 +20,7 @@ const Header = async () => {
       <nav className="container mx-auto px-4 py-4 flex items-center justify-between">
         <Link href={"/"}>
           <Image
-            src={"/logo.png"}
+            src={"/logoo.jpg"}
             alt="App logo"
             height={60}
             width={200}
